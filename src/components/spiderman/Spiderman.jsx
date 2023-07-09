e04e083ddb04ecdd4242298809bb75ac409ebb72
Toso <hello@iamtoso.com>
2023-07-09 20:27:30 +0300

    test

--- src/components/spiderman/Spiderman.jsx
+++ src/components/spiderman/Spiderman.jsx
@@ -7,13 +7,8 @@ import { HueSaturation, Bloom, EffectComposer, Glitch } from "@react-three/postp
 import { useLayoutEffect } from "react";
 gsap.registerPlugin(ScrollTrigger);
 
-
-
 export default function Spiderman({ timeline }) {
-  let  spiderman = useGLTF("./spiderman2099.glb");
-
-  
-
+  let spiderman = useGLTF("./spiderman2099.glb");
 
   // const { rotation, position, color,intensity,hue,saturation } = useControls({
   //   rotation: {
@@ -42,54 +37,50 @@ export default function Spiderman({ timeline }) {
 
   // });
 
-
-
   useLayoutEffect(() => {
-   
-    timeline && timeline.to(
-      spiderman.scene.rotation, {
-         duration: 3,
-         ease: "power2.inOut",
-          y:0,
-          onComplete:()=>{
+    timeline &&
+      timeline.to(
+        spiderman.scene.rotation,
+        {
+          duration: 3,
+          ease: "power2.inOut",
+          y: 0,
+          onComplete: () => {
             let RX = gsap.quickTo(spiderman.scene.rotation, "y", { duration: 0.5, ease: "sine" });
             window.addEventListener("mousemove", (e) => {
-              RX(e.pageX * 0.0001  );
+              RX(e.pageX * 0.0001);
             });
-          }
-        },0);
+          },
+        },
+        0
+      );
 
-    timeline && timeline.to(
-      spiderman.scene.position, {
-         duration: 2,
+    timeline &&
+      timeline.to(
+        spiderman.scene.position,
+        {
+          duration: 2,
           ease: "power2.inOut",
-          z:0,
-        },0);
-
+          z: 0,
+        },
+        0
+      );
   }, [timeline]);
-
-
-
 
   return (
     <Canvas camera={{ position: [0, -1, 8], fov: 30 }}>
-    <EffectComposer>
+      <EffectComposer>
         <Glitch delay={[1.5, 2.5]} duration={[0.4]} strength={2} active columns={0.1} ratio={0.85} />
         <HueSaturation hue={3} saturation={1.4} />
-      </EffectComposer>  
+      </EffectComposer>
 
       <pointLight color={"blue"} intensity={0.2} position={[-0.28, 0, 3.95]} />
       <pointLight color={"#F4E99B"} intensity={0.8} position={[0, 0, 3.95]} />
 
-      
       <Environment preset={null} files="./hdr4.hdr" blur={1} />
       <Sparkles speed={0.5} color={"blue"} size={2} count={100} scale={10} />
-  
 
-      <primitive  object={spiderman.scene} rotation={[0, 7, 0]} position={[0, -7.6, -1000]} scale={4.5} />
-
-
-
+      <primitive object={spiderman.scene} rotation={[0, 7, 0]} position={[0, -7.6, -1000]} scale={4.5} />
       {/* <mesh  position={[-1,0,0]} rotation={[0,.5,0]}>
   <planeGeometry args={[10,10]} />
   <MeshReflectorMaterial
